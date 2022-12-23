f77a446d29d4d9a4cafcf9f0511b2260ff87cd1a
Phát Nguyễn <vanphatct2002@gmail.com>
2022-12-24 02:53:22 +0700

    change connection string

--- back_end/src/config/index.js
+++ back_end/src/config/index.js
@@ -5,7 +5,8 @@ const config = {
         port: process.env.PORT || 3000
     },
     db: {
-        uri: process.env.MONGODB_URI || `mongodb://localhost:27017/${dbName}`
+        // uri: process.env.MONGODB_URI || `mongodb://localhost:27017/${dbName}`
+        uri: `mongodb+srv://admin:He8SXOUyRVdNmAvt@cluster0.c9re2w6.mongodb.net/${dbName}?retryWrites=true&w=majority`
     }
 }
 
